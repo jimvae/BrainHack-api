79d71da7d5b28022f70f4a24193ebb16b053ab07
Jim Vincent <e0425954@u.nus.edu>
2021-05-26 03:54:27 +0800

    adding port

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const db = knex({
     }
   });
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 app.post('/signin', handleSignin(db, bcrypt));
